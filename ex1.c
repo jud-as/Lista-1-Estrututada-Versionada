625e8cf969749f116bd3bc04a48d980bce7291e1
jud_as <guilhermeferreira082@gmail.com>
2023-08-21 22:37:06 -0300

    ex1 completo

--- ex1.c
+++ ex1.c
@@ -2,16 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Pessoa
-{
+struct Pessoa{
     char nome[30];
     int idade;
     float altura;
-};
+}Pessoa[];
 
+
+void setPessoa () {
+
+    printf("Nome: "); scanf("%s", &Pessoa->nome);
+    printf("\nIdade: "); scanf("%d", &Pessoa->idade);
+    printf("\nAltura: "); scanf("%f", &Pessoa->altura);
+    system("CLS");
+    printf("\nNome: %s", Pessoa->nome);
+    printf("\nIdade: %d", Pessoa->idade);
+    printf("\nAltura: %.2f", Pessoa->altura);
+}
 
 
 int main() {
-
-
+    setPessoa();
 }
